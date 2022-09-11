dace56aab6e1abaa0bdf3ccde6125006fc028079
Anton Volkov <awolf.986@gmail.com>
2022-09-11 22:56:06 +0300

    add version wp4 with new ngix config

---
+++ wp4/run.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+set -e
+
+# Note: update /etc/hosts separately, may require sudo privileges
+
+# Install mkcert, assumes brew already installed
+apt install mkcert
+
+# Initialize and create certificates
+mkcert -install
+
+mkdir -p ssl
+mkcert -key-file ssl/nginx.key -cert-file ssl/nginx.crt domain.com *.domain.com was.locl localhost 127.0.0.1 ::1 
+#chmod  -R 400 ssl
+# Run docker containers as daemons
+docker-compose up -d
+
+#Open default browser to https://example.com
+
+#open https://domain.com
+
